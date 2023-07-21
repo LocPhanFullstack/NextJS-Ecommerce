6c797f508bffb4a10b4eaae5003e0555f8af637c
LocPhanFullstack <phananhloc03102001@gmail.com>
2023-07-21 23:40:37 +0700

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,18 @@
-import Image from 'next/image';
+'use client';
 
-export default function Home() {
-    return <p>Hello</p>;
+import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
+
+export default function SetupPage() {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
+    return <div className="p-4">Root</div>;
 }
